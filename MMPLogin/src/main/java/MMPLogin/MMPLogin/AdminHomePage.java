7f84f91315d6485920853a7b9ecd79720698e86c
ktashamai <112670269+ktashamai@users.noreply.github.com>
2023-03-28 12:15:45 -0700

    Test

--- MMPLogin/src/main/java/MMPLogin/MMPLogin/AdminHomePage.java
+++ MMPLogin/src/main/java/MMPLogin/MMPLogin/AdminHomePage.java
@@ -7,6 +7,7 @@ public class AdminHomePage {
 	protected WebDriver driver;
 
 	public AdminHomePage(WebDriver driver1) {
+		//prathib
 		this.driver = driver1;
 		if (!driver.getTitle().equals("home")) {
 			throw new IllegalStateException("This is not Home Page of logged in user," +
